9f4efbc92d5e6c88f5a1972b1456fa711475019b
luanasava <luanasava@gmail.com>
2024-12-13 10:20:13 -0300

    Alteração de dependencia

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d889e97313424263fa4bf0f77cd3337fc3898b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TechChallengeFase3/obj/Debug/net8.0/TechChallengeFase3.Producer.AssemblyInfo.cs
+++ TechChallengeFase3/obj/Debug/net8.0/TechChallengeFase3.Producer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallengeFase3.Producer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d889e97313424263fa4bf0f77cd3337fc3898b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallengeFase3.Producer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallengeFase3.Producer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
