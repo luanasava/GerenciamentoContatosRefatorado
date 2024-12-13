b4b797263da81022709b75a927083e24503cde1b
luanasava <luanasava@gmail.com>
2024-12-13 11:16:36 -0300

    alterando dependencias

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac7903b8191fa8dd97cc77d5051cd4aab49a253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f52bfb702057c9dd2a1ff0e8d91d873676b078a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
