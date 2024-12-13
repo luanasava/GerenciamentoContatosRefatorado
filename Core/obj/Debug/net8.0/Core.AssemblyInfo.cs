7f52bfb702057c9dd2a1ff0e8d91d873676b078a
luanasava <luanasava@gmail.com>
2024-12-13 11:09:22 -0300

    incluindo dependencia de testes

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d47d4a6b962ab4cf4bcdc2dd09706074584d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac7903b8191fa8dd97cc77d5051cd4aab49a253")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
